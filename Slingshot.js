fbc59182cb08ebfff19a3b2baa827c544d07b591
sreekalaivanan <chathurvidya@gmail.com>
2020-12-29 10:22:40 +0000

    sree

--- Slingshot.js
+++ Slingshot.js
@@ -14,9 +14,15 @@ class SlingShot{
         World.add(world, this.sling);
     }
 
+    attach(body){
+        this.sling.bodyA = body;
+    }
+
     fly(){
         this.sling.bodyA = null;
     }
+
+    
 
     display(){
         image(this.sling1,200,20);
